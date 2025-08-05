a000d2e2e198806a2030fad0672c384ac529e602
pandeyshikha123 <pandeyshikha171@gmail.com>
2025-08-05 23:36:49 +0530

    update deployment url

--- SpendWiseAPI/Program.cs
+++ SpendWiseAPI/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200","https://spend-wise-fullstack.vercel.app") // update your frontend origin
+        policy.WithOrigins("http://localhost:4200","https://spend-wise-fullstack.vercel.app") 
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
